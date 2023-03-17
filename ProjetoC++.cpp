a5fbf9aa65e408d41ee09681e16c8f62605238fe
Ricardo-Viana <ricardo.viana18@gmail.com>
2023-03-17 11:14:38 -0300

    Adicionado verificação de dia, mes e ano

--- ProjetoC++.cpp
+++ ProjetoC++.cpp
@@ -6,37 +6,76 @@
 
 using namespace std;
 
+enum Meses {
+    JANEIRO = 1, FEVEREIRO, MARCO, ABRIL, MAIO,
+    JUNHO, JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+};
 short verificaAno(short ano);
 short verificaMes(short mes);
-short verificaDia(short dia, short mes);
+short verificaDia(short dia, short mes, short ano);
 short pegarDoisUltimosDigitos(short ano);
-short adicionarCodigoDoMes(short mes);
 bool verificaAnoBissexto(short ano);
 short codigoPorSeculo(short ano);
 
+
+
 int main()
-{
+{   
     string diasDaSemana[] = {"Sabado", "Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta"};
+    
     short const POSICOES = 3; // Uma data possui tres posições dd mm e aaaa, vai ajudar a inicializar o array de data
     short data[] = {0,0,0};
     // data[2] = ano, data[1] = mes, data[0] = dia
+    
     cout << "Digite o ano que deseja procurar o dia da semana (De 1700 a 2099)" << endl;
     cin >> data[2];
-    //ano = verificaAno(ano);
+    data[2] = verificaAno(data[2]);
+    
     cout << "Digite o mes que deseja procurar o dia da semana (De 1 a 12)" << endl;
     cin >> data[1];
-    //mes = verificaMes(mes);
-    cout << "Digite o dia que deseja procurar o dia da semana (De 1 a 31)" << endl;
-    cin >> data[0];
-    //dia = verificaDia(dia, mes);
+    data[1] = verificaMes(data[1]);
+   
+    data[0] = verificaDia(data[0], data[1], data[2]);
+    
     short calculoDia = pegarDoisUltimosDigitos(data[2]);
     calculoDia = calculoDia / 4;
     calculoDia += data[0]; // data[0] dia do mes
-    calculoDia += adicionarCodigoDoMes(data[1]);
+    
+    // Adicionar código do mês
+    switch (data[1]) {
+        case JANEIRO:
+        case OUTUBRO:
+            calculoDia += 1;
+            break;
+        case FEVEREIRO:
+        case MARCO:
+        case NOVEMBRO:
+            calculoDia += 4;
+            break;
+        case ABRIL:
+        case JULHO:
+            calculoDia += 0;
+            break;
+        case MAIO:
+            calculoDia += 2;
+            break;
+        case JUNHO:
+            calculoDia += 5;
+            break;
+        case AGOSTO:
+            calculoDia += 3;
+            break;
+        case SETEMBRO:
+        case DEZEMBRO:
+            calculoDia += 6;
+            break;
+    }
+
     // Subtrair um se o ano for bissexto e o mes janeiro ou fevereiro
     if (verificaAnoBissexto(data[2]) == true && data[1] == 1 || data[1] == 2) {
         calculoDia -= 1;
     }
+   
     calculoDia += codigoPorSeculo(data[2]);
     calculoDia += pegarDoisUltimosDigitos(data[2]);
     calculoDia = calculoDia % 7;
@@ -66,32 +105,6 @@ bool verificaAnoBissexto(short ano) {
 }
 
 
-
-short adicionarCodigoDoMes(short mes) {
-    switch (mes) {
-    case 1:
-    case 10:
-        return 1;
-    case 2:
-    case 3:
-    case 11:
-        return 4;
-    case 4:
-    case 7:
-        return 0;
-    case 5:
-        return 2;
-    case 6:
-        return 5;
-    case 8:
-        return 3;  
-    case 9:
-    case 12:
-        return 6;  
-    }
-    return 0;
-}
-
 short pegarDoisUltimosDigitos(short ano) {
     short doisUltDigitos = ano % 100;
     return doisUltDigitos;
@@ -103,7 +116,7 @@ short verificaAno(short ano) {
     }
     else {
         do {
-            std::cout << "Digite um ano válido (De 1700 a 2099)";
+            std::cout << "Digite um ano válido (De 1700 a 2099)" << endl;
             std::cin >> ano;
         } while (ano < 1700 || ano > 2099);
         return ano;
@@ -116,25 +129,54 @@ short verificaMes(short mes) {
     }
     else {
         do {
-            std::cout << "Digite um mês válido (De 1 a 12)";
+            std::cout << "Digite um mês válido (De 1 a 12)" << endl;
             std::cin >> mes;
         } while (mes < 1 || mes > 12);
         return mes;
     }
 }
 
-short verificaDia(short dia, short mes) {
-    return 1;
-    //if (mes % 2 == 0 && mes != 8) 
+short verificaDia(short dia, short mes, short ano) {
+    switch (mes) {
+        // meses com dia 31
+        case JANEIRO:
+        case MARCO:
+        case MAIO:
+        case JULHO:
+        case AGOSTO:
+        case OUTUBRO:
+        case DEZEMBRO:
+            do {
+                std::cout << "Digite um dia que deseja procurar o dia da semana (De 1 a 31)" << endl;
+                std::cin >> dia;
+            } while (dia < 1 || dia > 31);
+            return dia;
+        // meses com dia 30
+        case ABRIL:
+        case JUNHO:
+        case SETEMBRO:
+        case NOVEMBRO:
+            do {
+                std::cout << "Digite um dia que deseja procurar o dia da semana (De 1 a 30)" << endl;
+                std::cin >> dia;
+            } while (dia < 1 || dia > 30);
+            return dia;
+        case FEVEREIRO:
+            if (verificaAnoBissexto(ano)) {
+                do {
+                    std::cout << "Digite um dia que deseja procurar o dia da semana (De 1 a 29)" << endl;
+                    std::cin >> dia;
+                } while (dia < 1 || dia > 29);
+                return dia;
+            }
+            else {
+                do {
+                    std::cout << "Digite um dia que deseja procurar o dia da semana (De 1 a 28)" << endl;
+                    std::cin >> dia;
+                } while (dia < 1 || dia > 28);
+                return dia;
+            }
+    }
+
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
